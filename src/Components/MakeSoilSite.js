5e6cb38ed324651d5590a1fb7841570ecf3f3bbc
darshanbhandari123 <darshanbhandari78@gmail.com>
2018-04-29 08:08:52 -0400

    Made form structure

--- src/Components/MakeSoilSite.js
+++ src/Components/MakeSoilSite.js
@@ -3,3 +3,14 @@ import {View, StyleSheet,Text} from 'react-native';
 import { Button } from 'react-native';
 
 import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const SoilMaker = t.struct({
+	owner_name: t.String,
+	address: t.String,
+	description: t.String,
+	What_are_accepted: t.String,
+	What_are_not_accepted: t.String,	
+	terms: t.Boolean
+});
